81c9e0c4632bcce24e1e3615a9e62b7205e1c274
Steve Daneault <steve@daneault.com>
2019-03-09 12:57:04 -0500

    Added new metrics

--- load_data.py
+++ load_data.py
@@ -1,7 +1,7 @@
 from influx import InfluxDB
 import requests
-import json
 from datetime import datetime
+import time
 
 INFLUX_HOST = '192.168.1.215'
 INFLUX_PORT = 8086
@@ -23,19 +23,31 @@ def record_data(measurement, value, type, read_time):
                  time=read_time)
 
 
-resp = requests.get('http://192.168.1.92/production.json?details=1')
-print(json.dumps(resp.json(), indent=2))
+def get_solar_data():
+    print('Fetching solar data... ', end='')
+    resp = requests.get('http://192.168.1.92/production.json?details=1')
+    d = resp.json()
 
-d = resp.json()
+    inverter_stats = d['production'][0]
+    read_time = datetime.fromtimestamp(inverter_stats['readingTime'])
 
-inverterStats = d['production'][0]
-read_time = datetime.fromtimestamp(inverterStats['readingTime'])
+    record_data('wattHours', inverter_stats['wNow'], type='inverters', read_time=read_time)
+    record_data('activePanels', inverter_stats['activeCount'], type='inverters', read_time=read_time)
+    record_data('totalGeneration', inverter_stats['whLifetime'], type='inverters', read_time=read_time)
 
-record_data('wattHours', inverterStats['wNow'], type='inverters', read_time=read_time)
-record_data('activePanels', inverterStats['activeCount'], type='inverters', read_time=read_time)
-record_data('totalGeneration', inverterStats['whLifetime'], type='inverters', read_time=read_time)
+    eim_stats = d['production'][1]
+    read_time = datetime.fromtimestamp(eim_stats['readingTime'])
+    record_data('wattHours', eim_stats['wNow'], type='eim', read_time=read_time)
+    record_data('totalGeneration', eim_stats['whLifetime'], type='eim', read_time=read_time)
 
-eimStats = d['production'][1]
-read_time = datetime.fromtimestamp(eimStats['readingTime'])
-record_data('wattHours', eimStats['wNow'], type='eim', read_time=read_time)
-record_data('wattHours', eimStats['whLifetime'], type='eim', read_time=read_time)
+    for metric in ['varhLeadLifetime', 'varhLagLifetime', 'vahLifetime', 'rmsCurrent', 'rmsVoltage', 'reactPwr', 'apprntPwr', 'pwrFactor', 'whToday', 'whLastSevenDays', 'vahToday', 'varhLeadToday', 'varhLagToday']:
+        record_data(metric, eim_stats[metric], type='eim', read_time=read_time)
+
+    print('done.')
+
+
+if __name__ == '__main__':
+    while True:
+        get_solar_data()
+        time.sleep(60 * 5)
+
